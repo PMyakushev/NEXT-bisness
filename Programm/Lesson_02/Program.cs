93302653049a5a7f4ee7c8e84368316a438e5e45
Pavel <asercrut@yanex.ru>
2023-02-15 16:57:01 +0400

    lesson_003

--- Programm/Lesson_02/Program.cs
+++ Programm/Lesson_02/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int n = 5;
+// int[] array = new int[n];
+// for (int i = 0; i < 5; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("[" + string.Join(",", array)+ "]");
+// Console.WriteLine(array[3]);
+
+
+// //[4,5,3,1,2]
+// // 0(n)
+// // [1,2,3,4,5] -0(n* log n)
+// // ((5+1)/2)*5- 0*(1)
+
+// int summa = 0;
+// for (int i = 0; i < n; i++)
+//     summa +=array[i];
+
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for(int i= 1; i<=n; i++)
+// {
+//     for (int j = 1; j <= n; j++)
+//     {
+//         Console.Write(i*j+"\t");
+//     }
+//     Console.WriteLine();
+//}
+// 0(n^2)
+
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[n,n];
+// for(int i= 1; i<=n; i++)
+// {
+//     for (int j = 1; j <= n; j++)
+//     {
+//         if (i+j <=n)
+//             matrix[i,j]=i*j;
+//             matrix[j,i]=i*j;
+//     }
+//     Console.WriteLine();
+// }
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n,n];
+for(int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+    {
+    matrix[i,j]=(i+1)*(j+1);
+    matrix[j,i]=(i+1)*(j+1);
+    }
+    Console.WriteLine();
+}
+for(int i= 0; i<n; i++)
+{
+    for (int j = 0; j<n; j++)
+    {
+       Console.Write(matrix[i,j]+"/t");
+    //    Console.Write("  "); 
+    }
+    Console.WriteLine();
+}
